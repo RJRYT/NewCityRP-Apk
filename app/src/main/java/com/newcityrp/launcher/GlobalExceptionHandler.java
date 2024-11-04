59a547b832427af4e267e92d758246213f16929b
RJRYT <robinjratr@gmail.com>
2024-11-04 14:41:16 +0530

    try-catch method added in error handler

--- app/src/main/java/com/newcityrp/launcher/GlobalExceptionHandler.java
+++ app/src/main/java/com/newcityrp/launcher/GlobalExceptionHandler.java
@@ -25,30 +25,33 @@ public class GlobalExceptionHandler implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
-        FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
-        crashlytics.recordException(throwable);
+        try {
+            // Main logic of your custom error handler
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.recordException(throwable);
 
-        logManager = new LogManager(this.context);
-        logManager.logError("========uncaughtException========");
-        logManager.logError("Unhandled exception in thread " + thread.getName() + " " + throwable.toString());
+            logManager = new LogManager(this.context);
+            logManager.logError("========uncaughtException========");
+            logManager.logError(throwable.toString());
 
-        // Logcat log
-        Log.e(TAG, "Unhandled exception in thread " + thread.getName(), throwable);
+            Log.e(TAG, "Unhandled exception in thread " + thread.getName(), throwable);
 
-        // Increment the error count
-        int errorCount = sharedPreferences.getInt(ERROR_COUNT_KEY, 0) + 1;
-        sharedPreferences.edit().putInt(ERROR_COUNT_KEY, errorCount).apply();
+            int errorCount = sharedPreferences.getInt(ERROR_COUNT_KEY, 0) + 1;
+            sharedPreferences.edit().putInt(ERROR_COUNT_KEY, errorCount).apply();
 
-        // Show the toast message
-        Toast.makeText(this.context, "An unexpected error occurred. Restart the app.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this.context, "An unexpected error occurred. Restarting the app...", Toast.LENGTH_SHORT)
+                    .show();
 
-        // Check if the error count exceeds the max threshold
-        if (errorCount > MAX_ERROR_COUNT) {
-            // Reset error count and stop the app
-            sharedPreferences.edit().putInt(ERROR_COUNT_KEY, 0).apply();
-            Process.killProcess(Process.myPid());
-            System.exit(1);
-            return;
+            if (errorCount > MAX_ERROR_COUNT) {
+                sharedPreferences.edit().putInt(ERROR_COUNT_KEY, 0).apply();
+                Process.killProcess(Process.myPid());
+                System.exit(1);
+                return;
+            }
+        } catch (Exception e) {
+            // Log or handle any error occurring within the handler itself
+            Log.e(TAG, "Error in GlobalExceptionHandler: " + e.getMessage(), e);
         }
     }
+
 }
